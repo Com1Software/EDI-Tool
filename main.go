3bd7cb1495cd6a14c2a18f1d014959b3edbafec7
dave@com1software.com <dave@com1software.com>
2023-05-17 14:43:34 -0400

    update

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ import (
 
 //----------------------------------------------------------------
 func main() {
+	xip := "localhost"
+	//	workfolder := "/qa"
+	//	sourcefolder := "/sourcefiles"
+	//	destinationfolder := "/destinationfiles"
+	//	mapfolder := "/mapfiles"
+	//	schemafolder := "/schemafiles"
+	//	exefile := "/C1D0U484/C1D0U484.EXE"
+
 	fmt.Printf("EDI Tool - (c) Copyright Com1Software 1992-2023\n")
 	fmt.Printf("Repository : github.com/Com1Software/EDI-Tool\n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
@@ -19,8 +27,7 @@ func main() {
 	switch {
 	//-------------------------------------------------------------
 	case len(os.Args) == 2:
-
-		fmt.Println("Not")
+		fmt.Println("Command:")
 
 		//-------------------------------------------------------------
 	default:
@@ -29,30 +36,29 @@ func main() {
 		fmt.Println("Listening on port 8080")
 		fmt.Println("")
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			xdata := InitPage()
+			xdata := InitPage(xip)
 			fmt.Fprint(w, xdata)
 		})
 		//------------------------------------------------ About Page Handler
 		http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-			xdata := AboutPage()
+			xdata := AboutPage(xip)
 			fmt.Fprint(w, xdata)
 		})
-		//------------------------------------------------ PlayGround Page Handler
-		http.HandleFunc("/playground", func(w http.ResponseWriter, r *http.Request) {
-			xdata := PlayGroundPage()
+		//------------------------------------------------ Settings Page Handler
+		http.HandleFunc("/settings", func(w http.ResponseWriter, r *http.Request) {
+			xdata := SettingsPage(xip)
 			fmt.Fprint(w, xdata)
 		})
-		//------------------------------------------------ Display Page Handler
-		http.HandleFunc("/display", func(w http.ResponseWriter, r *http.Request) {
-			xdata := DisplayPage()
+		//------------------------------------------------ Projects Page Handler
+		http.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+			xdata := ProjectsPage(xip)
 			fmt.Fprint(w, xdata)
 		})
-		//------------------------------------------------ Test Page Handler
-		http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-			xdata := TestPage()
+		//------------------------------------------------ Examples Page Handler
+		http.HandleFunc("/examples", func(w http.ResponseWriter, r *http.Request) {
+			xdata := ExamplesPage(xip)
 			fmt.Fprint(w, xdata)
 		})
-
 		//------------------------------------------------- Static Handler Handler
 		fs := http.FileServer(http.Dir("static/"))
 		http.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -85,23 +91,16 @@ func Openbrowser(url string) error {
 	return exec.Command(cmd, args...).Start()
 }
 
-func InitPage() string {
-	//---------------------------------------------------------------------------
-	//----------------------------------------------------------------------------
-	xip := "localhost"
+//----------------------------------------------------- Init Page
+func InitPage(xip string) string {
 	xxip := ""
 	xdata := "<!DOCTYPE html>"
 	xdata = xdata + "<html>"
 	xdata = xdata + "<head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<title>EDI Tool -  (c) Copyright Com1Software 1992-2023</title>"
-	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
-	//------------------------------------------------------------------------
+	xdata = xdata + "<title>EDI Tool</title>"
 	xdata = xdata + "</head>"
-	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
 	xdata = xdata + "<H1>EDI Tool</H1>"
-	//---------
 	host, _ := os.Hostname()
 	addrs, _ := net.LookupIP(host)
 	for _, addr := range addrs {
@@ -109,232 +108,97 @@ func InitPage() string {
 			xxip = fmt.Sprintf("%s", ipv4)
 		}
 	}
-	xip = "localhost"
 	xdata = xdata + "<div id='txtdt'></div>"
 	xdata = DateTimeDisplay(xdata)
 	xdata = xdata + "<p> Host Port IP : " + xip
 	xdata = xdata + "<BR> Machine IP : " + xxip + "</p>"
+	xdata = xdata + "  <A HREF='http://" + xip + ":8080/examples'> [ Examples ] </A>  "
+	xdata = xdata + "  <A HREF='http://" + xip + ":8080/projects'> [ Projects ] </A>  "
+	xdata = xdata + "  <A HREF='http://" + xip + ":8080/settings'> [ Settings ] </A>  "
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/about'> [ About ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/playground'> [ PlayGround ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/display'> [ Display ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/test'> [ Testing ] </A>  "
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/static/index.html'> [ Documentation ] </A>  "
 	xdata = xdata + "<BR><BR><small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
-	//------------------------------------------------------------------------
 	xdata = xdata + " </body>"
 	xdata = xdata + " </html>"
 	return xdata
 }
 
-//----------------------------------------------------------------
-func AboutPage() string {
-	//---------------------------------------------------------------------------
-
-	//----------------------------------------------------------------------------
+//--------------------------------- Settings
+func SettingsPage(xip string) string {
 	xdata := "<!DOCTYPE html>"
 	xdata = xdata + "<html>"
 	xdata = xdata + "<head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<title>About Page</title>"
-	//------------------------------------------------------------------------
+	xdata = xdata + "<title>EDI Tool Projects</title>"
 	xdata = DateTimeDisplay(xdata)
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: lightblue;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p {"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
 	xdata = xdata + "</head>"
-	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
-	xdata = xdata + "<p>Go Web Server</p>"
+	xdata = xdata + "<H1>EDI Tool Projects</H1>"
 	xdata = xdata + "<div id='txtdt'></div>"
-	//---------
-	xdata = xdata + "  <A HREF='https://github.com/Com1Software/Test-GoWebServer'> [ GoWebServer GitHub Repository ] </A>  "
-	xdata = xdata + "<BR>"
-	xdata = xdata + "<small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
-	//------------------------------------------------------------------------
-
-	//------------------------------------------------------------------------
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
+	xdata = xdata + "<br><br>"
+	xdata = xdata + "<A HREF='http://" + xip + ":8080'> [ Main Menu ] </A>  "
+	xdata = xdata + "<BR><BR><small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
+	xdata = xdata + "</body>"
+	xdata = xdata + "</html>"
 	return xdata
 
 }
 
-//----------------------------------------------------------------
-func TestPage() string {
-	//---------------------------------------------------------------------------
-
-	//----------------------------------------------------------------------------
+//--------------------------------- Examples
+func ExamplesPage(xip string) string {
 	xdata := "<!DOCTYPE html>"
 	xdata = xdata + "<html>"
 	xdata = xdata + "<head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<title>Test Page</title>"
-	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
-	//------------------------------------------------------------------------
-	xdata = LoopDisplay(xdata)
-
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: grey;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p1 {"
-	xdata = xdata + "color: green;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p2 {"
-	xdata = xdata + "color: red;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "	div {"
-	xdata = xdata + "color: white;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
-	xdata = xdata + "</head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<body onload='startLoop()'>"
-	xdata = xdata + "<H1>Test Page</H1>"
-	xdata = xdata + "<div id='txtloop'></div>"
-	//---------
-	xdata = xdata + "<center>"
-	xdata = xdata + "<p1>Go Web Server</p1>"
-	xdata = xdata + "<BR>"
-	xdata = xdata + "<p2>Go Web Server</p2>"
-	xdata = xdata + "</center>"
-
-	//------------------------------------------------------------------------
-	//	xdata = xdata + "  <A HREF='http://" + xip + ":8080'> [ Return to Start Page ] </A>  "
-
-	//------------------------------------------------------------------------
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
-	return xdata
-
-}
-
-//----------------------------------------------------------------
-func DisplayPage() string {
-	//---------------------------------------------------------------------------
-
-	//----------------------------------------------------------------------------
-	xdata := "<!DOCTYPE html>"
-	xdata = xdata + "<html>"
-	xdata = xdata + "<head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<title>Display Page</title>"
-	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
-	//------------------------------------------------------------------------
+	xdata = xdata + "<title>EDI Tool Projects</title>"
 	xdata = DateTimeDisplay(xdata)
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: black;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p1 {"
-	xdata = xdata + "color: green;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p2 {"
-	xdata = xdata + "color: red;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "	div {"
-	xdata = xdata + "color: white;"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
 	xdata = xdata + "</head>"
-	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
-	xdata = xdata + "<H1>Display Page</H1>"
+	xdata = xdata + "<H1>EDI Tool Projects</H1>"
 	xdata = xdata + "<div id='txtdt'></div>"
-	//---------
-	xdata = xdata + "<center>"
-	xdata = xdata + "<p1>Go Web Server</p1>"
-	xdata = xdata + "<BR>"
-	xdata = xdata + "<p2>Go Web Server</p2>"
-	xdata = xdata + "</center>"
-
-	//------------------------------------------------------------------------
-
-	//------------------------------------------------------------------------
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
+	xdata = xdata + "<br><br>"
+	xdata = xdata + "<A HREF='http://" + xip + ":8080'> [ Main Menu ] </A>  "
+	xdata = xdata + "<BR><BR><small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
+	xdata = xdata + "</body>"
+	xdata = xdata + "</html>"
 	return xdata
 
 }
 
-//----------------------------------------------------------------
-//----------------------------------------------------------------
-func PlayGroundPage() string {
-	//---------------------------------------------------------------------------
-
-	//----------------------------------------------------------------------------
+//--------------------------------- Projects
+func ProjectsPage(xip string) string {
 	xdata := "<!DOCTYPE html>"
 	xdata = xdata + "<html>"
 	xdata = xdata + "<head>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "<title>The PlayGround</title>"
-	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
-	//------------------------------------------------------------------------
+	xdata = xdata + "<title>EDI Tool Projects</title>"
 	xdata = DateTimeDisplay(xdata)
-	//------------------------------------------------------------------------
 	xdata = xdata + "</head>"
-	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
-	xdata = xdata + "<p>The PlayGround.</p>"
-	xdata = xdata + "<div id='txt'></div>"
-	xdata = xdata + "<p id='demo1'></p>"
-	xdata = xdata + "<button onclick='myFunction1()'>Click me 1</button> - "
-	xdata = xdata + "<button onclick='myFunction2()'>Click me 2</button>"
-	xdata = xdata + "<p id='demo2'></p>"
+	xdata = xdata + "<H1>EDI Tool Projects</H1>"
 	xdata = xdata + "<div id='txtdt'></div>"
+	xdata = xdata + "<br><br>"
+	xdata = xdata + "<A HREF='http://" + xip + ":8080'> [ Main Menu ] </A>  "
+	xdata = xdata + "<BR><BR><small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
+	xdata = xdata + "</body>"
+	xdata = xdata + "</html>"
+	return xdata
 
-	//---------
-	xdata = xdata + "Go Web Server"
+}
 
-	//------------------------------------------------------------------------
-	xdata = xdata + "<script>"
-	//----------
-	xdata = xdata + " function myFunction1() {"
-	xdata = xdata + "document.getElementById('demo1').innerHTML = 'Hello<BR> World';"
-	xdata = xdata + "document.getElementById('demo2').innerHTML = 'Hello<BR> World';"
-	xdata = xdata + "  }"
-	//----------
-	xdata = xdata + " function myFunction2() {"
-	xdata = xdata + "document.getElementById('demo1').innerHTML = 'Hello World 2 Upper screen stuff';"
-	xdata = xdata + "document.getElementById('demo2').innerHTML = 'Hello World 2 Lower screen stuff';"
-	xdata = xdata + "  }"
-	//----------
-	xdata = xdata + "</script>"
-	//------------------------------------------------------------------------
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
+//--------------------------------- About
+func AboutPage(xip string) string {
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	xdata = xdata + "<title>EDI Tool Projects</title>"
+	xdata = DateTimeDisplay(xdata)
+	xdata = xdata + "</head>"
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<H1>EDI Tool About</H1>"
+	xdata = xdata + "<div id='txtdt'></div>"
+	xdata = xdata + "<br><br>"
+	xdata = xdata + "<A HREF='http://" + xip + ":8080'> [ Main Menu ] </A>  "
+	xdata = xdata + "<BR><BR><small>EDI Tool -  (c) Copyright Com1Software 1992-2023</small>"
+	xdata = xdata + "</body>"
+	xdata = xdata + "</html>"
 	return xdata
 
 }
@@ -348,7 +212,7 @@ func DateTimeDisplay(xdata string) string {
 	xdata = xdata + "  var h = today.getHours();"
 	xdata = xdata + "  var m = today.getMinutes();"
 	xdata = xdata + "  var s = today.getSeconds();"
-	xdata = xdata + "  var ampm = h >= 12 ? 'pm' : 'am';"
+	xdata = xdata + "  var ampm = h >= 12 ? 'pm.' : 'am.';"
 	xdata = xdata + "  var mo = today.getMonth();"
 	xdata = xdata + "  var dm = today.getDate();"
 	xdata = xdata + "  var yr = today.getFullYear();"
@@ -418,7 +282,7 @@ func DateTimeDisplay(xdata string) string {
 	xdata = xdata + "       break;"
 	xdata = xdata + "}"
 	//  -------------------------------------------------------------------
-	xdata = xdata + "  document.getElementById('txtdt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
+	xdata = xdata + "  document.getElementById('txtdt').innerHTML = ' Date : '+day+', '+month+' '+dm+', '+yr+'.  - Time : '+h + ':' + m + ':' + s+' '+ampm;"
 	xdata = xdata + "  var t = setTimeout(startTime, 500);"
 	xdata = xdata + "}"
 	//----------
@@ -430,19 +294,6 @@ func DateTimeDisplay(xdata string) string {
 	xdata = xdata + "function checkTimeH(i) {"
 	xdata = xdata + "  if (i > 12) {i = i -12};"
 	xdata = xdata + "  return i;"
-	xdata = xdata + "}"
-	xdata = xdata + "</script>"
-	return xdata
-
-}
-
-func LoopDisplay(xdata string) string {
-	//------------------------------------------------------------------------
-	xdata = xdata + "<script>"
-	xdata = xdata + "function startLoop() {"
-	//  -------------------------------------------------------------------
-	xdata = xdata + "  document.getElementById('txtloop').innerHTML = Math.random();"
-	xdata = xdata + "  var t = setTimeout(startLoop, 500);"
 	xdata = xdata + "}"
 	xdata = xdata + "</script>"
 	return xdata
